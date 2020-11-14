702091e99f82d0138958d4e1aef0b902b4322bb5
Alexandr Lukyanets <alexlukyanets18@gmail.com>
2020-11-15 01:53:36 +0200

    Connect Postgres

--- test_task/settings.py
+++ test_task/settings.py
@@ -78,12 +78,12 @@ WSGI_APPLICATION = 'test_task.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
-        "USER": os.environ.get("SQL_USER", "user"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
-        "HOST": os.environ.get("SQL_HOST", "localhost"),
-        "PORT": os.environ.get("SQL_PORT", "5432"),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        "NAME": "django_db",
+        "USER": "postgres",
+        "PASSWORD": "wise2012A",
+        "HOST": "127.0.0.1",
+        "PORT": "5432"
     }
 }
 
